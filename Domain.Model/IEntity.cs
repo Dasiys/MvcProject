99e597fab92a65831a3e1db804bddb891bdc14d2
Dasiys <yeweimi@iCloud.com>
2017-04-20 13:46:46 +0800

    增加Model

---
+++ Domain.Model/IEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    interface IEntity
+    {
+        int Id { get; set; };
+    }
+    public class EntityBase:IEntity
+    {
+        /// <summary>
+        /// 设置或获取Id
+        /// </summary>
+        public int Id { set; get; }
+    }
+}
---
+++ Domain.Model/Permissions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public class Permissions:EntityBase
+    {
+        /// <summary>
+        /// 设置或获取权限名称
+        /// </summary>
+        public string Name { set; get; }
+
+        /// <summary>
+        /// 设置或获取是否为父级权限
+        /// </summary>
+        public bool IsParent { set; get; }
+
+        /// <summary>
+        /// 设置或获取父级Id
+        /// </summary>
+        public int ParentId { set; get; }
+    }
+}
