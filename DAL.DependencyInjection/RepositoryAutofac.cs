9295efed50cf108c72a4e023f87db85e33b057a5
Dasiys <yeweimi@iCloud.com>
2017-04-21 14:42:08 +0800

    Autofac依赖注入

---
+++ DAL.DependencyInjection/RepositoryAutofac.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Model.Repository.Permissions;
+using Model.Repository.Role;
+using Model.Repository.RolePermissionsMap;
+using Repository.Permissions;
+using Repository.Role;
+using Repository.RolePermissionsMap;
+
+namespace DAL.DependencyInjection
+{
+    public static class RepositoryAutofac
+    {
+        public static ContainerBuilder RepositoryInjection(this ContainerBuilder builder)
+        {
+            builder.RegisterType<PermissionsRepository>().As<IPermissionRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<RoleRepository>().As<IRoleRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<RolePermissionsMapRepository>().As<IRolePermissionsMapRepository>().InstancePerLifetimeScope();
+            return builder;
+        }
+    }
+}
---
+++ DAL.DependencyInjection/UnitOfWorkAutofac.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Infrastructure.UnitOfWork;
+
+namespace DAL.DependencyInjection
+{
+    public static class UnitOfWorkAutofac
+    {
+        public static ContainerBuilder UnitOfWorkInjection(this ContainerBuilder builder)
+        {
+            builder.RegisterType<Infrastructure.UnitOfWork.UnitOfWork>().As<IUnitOfWork>().InstancePerLifetimeScope();
+            return builder;
+        }
+    }
+}
