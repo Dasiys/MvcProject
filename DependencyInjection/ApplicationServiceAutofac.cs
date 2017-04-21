7823781f9d4b148446f69316f47cf61384c4df5a
Dasiys <yeweimi@iCloud.com>
2017-04-21 14:51:33 +0800

    Autofac注入

---
+++ DependencyInjection/ApplicationServiceAutofac.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationInterface;
+using Autofac;
+using Re;
+
+namespace DependencyInjection
+{
+    public static class ApplicationServiceAutofac
+    {
+        public static ContainerBuilder ApplicationInjection(this ContainerBuilder builder)
+        {
+            builder.RegisterType<PermissionsService>().As<IPermissionsService>().InstancePerLifetimeScope();
+            builder.RegisterType<RoleService>().As<IRoleService>().InstancePerLifetimeScope();
+            builder.RegisterType<RolePermissionsMapService>().As<IRolePermissionsMapService>().InstancePerLifetimeScope();
+            return builder;
+        }
+    }
+}
