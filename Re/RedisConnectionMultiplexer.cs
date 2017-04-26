bad55cc9c32e4a0e24674f2fe9004ac38f9ac609
Dasiys <yeweimi@iCloud.com>
2017-04-26 16:54:25 +0800

    添加redis

---
+++ Re/RedisConnectionMultiplexer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace Re
+{
+    public class RedisConnectionMultiplexer
+    {
+        private static ConnectionMultiplexer _redis;
+        private static object _locker = new object();
+        public static ConnectionMultiplexer Manager
+        {
+            get {
+                if (_redis == null)
+                {
+                    lock (_locker)
+                    {
+                        _redis = GetManager();
+                        return _redis;
+                    }
+                }
+                return _redis;
+            }
+        }
+
+        public static ConnectionMultiplexer GetManager(string connectionString=null)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                connectionString = "localhost";
+            }
+            return ConnectionMultiplexer.Connect(connectionString);
+        }
+    }
+}
